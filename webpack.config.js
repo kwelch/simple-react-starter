1b525decfda0ebda67da72b61c2ce50ea193dd9f
Kyle Welch <kwelch0626@gmail.com>
2017-03-30 16:05:49 -0500

    Init commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  target: 'web',
+  devtool: 'cheap-eval-source-map',
+  entry: './src/index.js',
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve('./dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      // {
+      //   test: /\.js$/,
+      //   enforce: 'pre',
+      //   exclude: ['node_modules'],
+      //   loader: 'eslint-loader',
+      // },
+      {
+        test: /\.js$/,
+        exclude: ['node_modules'],
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    // build optimization plugins
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+      filename: 'common.[hash].js',
+    }),
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+};
