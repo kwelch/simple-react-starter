7e8eee63b0686b91a5d9d792a6be2c67a8bb97a5
Kyle Welch <kwelch0626@gmail.com>
2017-07-26 16:30:34 -0500

    Enhancements for full feature
    - added production builds
    - added deploy to gh-pages
    - fixed up eslint

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -6,34 +6,18 @@ module.exports = () => ({
   target: 'web',
   devtool: 'cheap-eval-source-map',
   entry: {
-    main: [
-      'babel-polyfill',
-
-      // activate HMR for React
-      'react-hot-loader/patch',
-
-      // bundle the client for webpack-dev-server
-      // and connect to the provided endpoint
-      'webpack-dev-server/client?http://localhost:3000',
-
-      // bundle the client for hot reloading
-      // only- means to only hot reload for successful updates
-      'webpack/hot/only-dev-server',
-
-      './src/index.js',
-    ],
+    main: ['babel-polyfill', './src/index.js'],
   },
   output: {
     filename: '[name].[hash].js',
     path: path.resolve('./dist'),
-    publicPath: '/',
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: ['node_modules'],
-        use: [{ loader: 'babel-loader' }],
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
@@ -56,15 +40,22 @@ module.exports = () => ({
     ],
   },
   plugins: [
-    // enable HMR globally
-    new webpack.HotModuleReplacementPlugin(),
-
-    // prints more readable module names in the browser console on HMR updates
-
-    // do not emit compiled assets that include errors
-    new webpack.NoEmitOnErrorsPlugin(),
-
-    new webpack.NamedModulesPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      beautify: false,
+      mangle: {
+        screw_ie8: true,
+        keep_fnames: true,
+      },
+      compress: {
+        screw_ie8: true,
+      },
+      comments: false,
+    }),
     // build optimization plugins
     new webpack.optimize.CommonsChunkPlugin({
       name: 'common',
@@ -74,16 +65,4 @@ module.exports = () => ({
       template: 'index.html',
     }),
   ],
-  devServer: {
-    host: 'localhost',
-    port: 3000,
-
-    // respond to 404s with index.html
-    historyApiFallback: true,
-
-    // enable HMR on the server
-    hot: true,
-
-    open: true,
-  },
 });
